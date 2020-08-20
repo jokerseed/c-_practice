ef70842a33bebfaa41aa5626525b4e0466ad1049
jokerseed <573202560@qq.com>
2020-08-20 21:08:17 +0800

    。

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            //穿插值
+            // string a = "猪";
+            // Console.WriteLine($"我是大帅{a}");
+
+            //取出空格
+            // string greeting = "      Hello World!       ";
+            // Console.WriteLine($"[{greeting}]");
+
+            // string trimmedGreeting = greeting.TrimStart();
+            // Console.WriteLine($"[{trimmedGreeting}]");
+
+            // trimmedGreeting = greeting.TrimEnd();
+            // Console.WriteLine($"[{trimmedGreeting}]");
+
+            // trimmedGreeting = greeting.Trim();
+            // Console.WriteLine($"[{trimmedGreeting}]");
+
+            //字符串替换   字符串全大写和全小写
+            // string sayHello = "Hello World!";
+            // Console.WriteLine(sayHello);
+            // sayHello = sayHello.Replace("Hello", "Greetings");
+            // Console.WriteLine(sayHello);
+
+            // Console.WriteLine(sayHello.ToUpper() + "-----" + sayHello.ToLower());
+
+            //字符串中是否包含子串   字符串是否以某一子串开头或结尾
+            // string songLyrics = "You say goodbye, and I say hello";
+            // Console.WriteLine(songLyrics.Contains("goodbye"));
+            // Console.WriteLine(songLyrics.Contains("greetings"));
+
+            // Console.WriteLine(songLyrics.StartsWith("You"));
+            // Console.WriteLine(songLyrics.EndsWith("hello"));
+
+            //双精度浮点数范围
+            // Console.WriteLine(double.MaxValue + "~" + double.MinValue);
+
+            // double third = 1.0 / 3.0;
+            // Console.WriteLine(third);
+
+            //十进制数
+            // Console.WriteLine(decimal.MinValue + "~" + decimal.MaxValue);
+            // double a = 1.0;
+            // double b = 3.0;
+            // Console.WriteLine(a / b);
+
+            // decimal c = 1.0M;
+            // decimal d = 3.0M;
+            // Console.WriteLine(c / d);
+
+            // double radius = 2.50;
+            // double area = Math.PI * radius * radius;
+            // Console.WriteLine(area);
+
+            //List遍历
+            // var names = new List<string> { "<name>", "Ana", "Felipe" };
+            // foreach (var name in names)
+            // {
+            //     Console.WriteLine($"Hello {name.ToUpper()}!");
+            // }
+        }
+    }
+}
