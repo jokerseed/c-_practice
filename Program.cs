ea2e17fd4a472223bdc695350bbbf4260120b219
573202560@qq.com <LIUquan123>
2020-08-24 22:37:38 +0800

    。

--- Program.cs
+++ Program.cs
@@ -128,12 +128,32 @@ namespace c
             // Console.WriteLine(typeof(Person).GetTypeInfo());
             // Console.WriteLine(typeof(Person).GetTypeInfo().GetCustomAttributes());
             // Console.WriteLine(typeof(Person).GetFields());
-            // Person p = new Person("xx");
+            Person p = new Person("xx");
             // Console.WriteLine(p);
             // Console.WriteLine(p.GetType());
             // Console.WriteLine(p.GetType().GetTypeInfo().GetCustomAttributes());
             // Console.WriteLine(p.GetType().GetProperties());
             // Console.WriteLine(p.GetType().GetFields());
+
+
+            // var t = p.GetType().GetTypeInfo().GetCustomAttributes();
+            // foreach (var temp in t)
+            // {
+            //     System.Console.WriteLine(temp);
+            // }
+
+            // var list = new List<int>();
+            // list.Add(1);
+
+            // list.Add(1);
+            // list.Add(1);
+            // list.Add(1);
+
+            // list.Add(1);
+            // list.Add(1);
+            // list.Add(1);
+            // list.Add(1);
+            // list.ForEach(f => System.Console.WriteLine(f));
         }
 
         // Program.cs
