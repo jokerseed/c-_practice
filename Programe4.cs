e73764bee9cf7ffbce052807af76306a02481e0b
jokerseed <573202560@qq.com>
2020-09-09 16:42:16 +0800

    。

--- Programe4.cs
+++ Programe4.cs
@@ -8,6 +8,7 @@ using System.Timers;
 using System.Net.Http;
 using System.Diagnostics;
 using System.Threading;
+using System.Collections;
 
 namespace c
 {
@@ -15,7 +16,27 @@ namespace c
     {
         public void Init()
         {
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            d["one"] = 10;
+            d["two"] = 110;
+            d["three"] = 1110;
+            d["four"] = 11110;
+            d["five"] = 111110;
 
+            //队列
+            Queue<int> q = new Queue<int>();
+
+            //栈
+            Stack<int> s = new Stack<int>();
+
+            //按键排序的键值对
+            SortedList<int, int> sl = new SortedList<int, int>();
+
+            ArrayList l = new ArrayList();
+            l.Add(10);
+            l.Add(11);
+            l.Add(12);
+            l.Add("dasda");
         }
     }
 }
